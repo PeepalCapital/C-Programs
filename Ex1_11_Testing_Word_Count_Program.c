8ec6f80984e6d862dacd0e3f6347b3ff16f922f5
Deepak Venkatesh <peepalcapital@gmail.com>
2022-06-15 20:43:58 -0700

    commit

---
+++ Ex1_11_Testing_Word_Count_Program.c
@@ -0,0 +1,6 @@
+/* Test the word count program to uncover bugs.
+For me the example of the word count program doesn't work.
+The file is mentiones as 'word_counting.c'
+Will try to solve it again later.
+And follow up with this exercise.
+I guess this won't be a program but rather a testing sequence. */
