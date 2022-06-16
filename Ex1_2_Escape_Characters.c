0a929060a0aeef176c65124d8c8a3b1670723cd1
Deepak Venkatesh <peepalcapital@gmail.com>
2022-06-15 18:07:59 -0700

    commit

---
+++ Ex1_2_Escape_Characters.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void)
+
+{
+    printf("Escape sequences as per KNK book\n\n");
+    printf("newline \\n is \n this \n");
+    printf("tab \\t is \t this \n");
+    printf("blankspace \\b is \b this \n");
+    printf("bell \\a is \a this \n");
+    printf("form feed \\f is \f this \n");
+    printf("carriage return \\r is \r this \n");
+    printf("vertical tab \\v is \v this \n");
+    printf("backslash \\is\\ this \n");
+    printf("question mark \?is? this \n");
+    printf("single quote \\'is' this \n");
+    printf("double quote \"is\" this \n");
+
+    return 0;
+}
