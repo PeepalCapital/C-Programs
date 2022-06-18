2324491306f553bb2da8d4000d58ce25d37b1e56
Deepak Venkatesh <peepalcapital@gmail.com>
2022-06-17 21:03:00 -0700

    commit

---
+++ Ex1_13_histogram_word.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/*program is incomplete*/
+
+int main(void)
+{
+    int i, c;
+
+    while((c = getchar()) != EOF)
+        ++i;
+    
+    printf("%d", i);
+
+    return 0;
+    
+}
+
