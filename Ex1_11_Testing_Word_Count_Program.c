60eff114684150d37d49bc76c215463020c1f52e
Deepak Venkatesh <peepalcapital@gmail.com>
2022-06-18 20:52:38 -0700

    commit

--- Ex1_11_Testing_Word_Count_Program.c
+++ Ex1_11_Testing_Word_Count_Program.c
@@ -3,4 +3,14 @@ For me the example of the word count program doesn't work.
 The file is mentiones as 'word_counting.c'
 Will try to solve it again later.
 And follow up with this exercise.
-I guess this won't be a program but rather a testing sequence. */
+I guess this won't be a program but rather a testing sequence. 
+
+1. Any special character tests such which are not \t, \b and \n such as .,;:'"-
+2. But there could proper nouns which could include ' and -
+2. (1) can be solved but (2) is very contextual and I am unsure how that can be solved with my current skill set
+
+
+
+
+
+*/
